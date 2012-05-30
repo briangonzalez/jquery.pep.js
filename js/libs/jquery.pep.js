dda9c0b87eee6ee0b4164c3fe9a6180823003ff7
Jonathan Brumley <cayasso@gmail.com>
2012-05-29 21:15:44 -0600

    Adding fix for constranToParent

--- js/libs/jquery.pep.js
+++ js/libs/jquery.pep.js
@@ -1,10 +1,10 @@
 /*******************************************************************
- * Pep! (jquery.pep.js) 
+ * Pep! (jquery.pep.js)
  * [ Version 0.2 ]
- * ---------------------------------------------------------- 
+ * ----------------------------------------------------------
  * Copyright 2012, Brian Gonzalez
  * Dual licensed under the MIT or GPL Version 2 licenses.
- *  
+ *
  *
  *    Dependencies:
  *        - jQuery
@@ -15,13 +15,13 @@
     // Create the defaults once
     var pluginName = 'pep',
         defaults = {
-            debug:                  false,
+            debug:                  true,
             activeClass:            'active',
             multiplier:             1,
             stopEvents:             "",
             // get more css         ease params from [ http://matthewlein.com/ceaser/ ]
             cssEaseString:          "cubic-bezier(0.210, 1, 0.220, 1.000)",
-            cssEaseDuration:        1000,
+            cssEaseDuration:        3000,
             constrainToWindow:      false,
             constrainToParent:      false,          // EXPERIMENTAL! use with caution. you've been warned.
             shouldEase:             true,
@@ -30,14 +30,15 @@
             stop:                   function(){},
             rest:                   function(){}
         },
+        vendors = ['-webkit-', '-moz-', '-ms-', '-o-', ''],
         disable = false;
 
     // The actual plugin constructor
     function Pep( element, options ) {
-        this.el = element;
-        this.options = $.extend( {}, defaults, options) ;
-        this._defaults = defaults;
-        this._name = "pep";
+        this.el =                   element;
+        this.options =              $.extend( {}, defaults, options) ;
+        this._defaults =            defaults;
+        this._name =                "pep";
         this._scale =               1;
         this._active =              false;
         this._start =               false;
@@ -79,16 +80,23 @@
         event.preventDefault();
         var self              = this;
         var $this             = $(this.el);
-        if( $this.hasClass( self.options.activeClass ) ) stopping();   
+        var $parent           = $this.parent();
+        var e                 = self._isTouch() ? event.originalEvent.touches[0] : event;
+
+        if( $this.hasClass( self.options.activeClass ) ) stopping();
         $this.addClass( this.options.activeClass );
-        this._x               = self._isTouch() ? event.originalEvent.pageX : event.pageX;
-        this._y               = self._isTouch() ? event.originalEvent.pageY : event.pageY;
+        this._x               = e.pageX;
+        this._y               = e.pageY;
         this._startX          = this._x;
         this._startY          = this._y;
         this._start           = true;
         this._active          = true;
         this._started         = false;
         this._moveEvent       = null;
+        this._width           = $this.outerWidth();
+        this._height          = $this.outerHeight();
+        this._maxX            = $parent.width() - this._width;
+        this._maxY            = $parent.height() - this._height;
         self._log( this._startTrigger );
 
         // remove CSS3 animation
@@ -126,9 +134,9 @@
   
           // fire user's drag event.
           self.options.drag(event, self);
-
-          var curX    = (self._isTouch() ? event.originalEvent.touches[0].pageX : event.pageX);
-          var curY    = (self._isTouch() ? event.originalEvent.touches[0].pageY : event.pageY);
+          var e       = self._isTouch() ? event.originalEvent.touches[0] : event;
+          var curX    = e.pageX;
+          var curY    = e.pageY;
 
           // put our target element exectly where it is...
           // but make it movable (pos absolute)
@@ -148,45 +156,29 @@
           var dx      = ( self._start ) ? 0 : curX - self._x;
           var dy      = ( self._start ) ? 0 : curY - self._y;
           var mult    = self.options.multiplier;
-          var xOp     = ( dx >= 0 ) ? "+=" + Math.abs(dx / self._scale)*mult : "-=" + Math.abs(dx / self._scale)*mult;
-          var yOp     = ( dy >= 0 ) ? "+=" + Math.abs(dy / self._scale)*mult : "-=" + Math.abs(dy / self._scale)*mult;
-          
-          if (self.options.constrainToParent) {
-            var pos     = $this.position();
-            var $parent = $this.parent();
-
-            pos.right   = $parent.width() - $this.width();
-            pos.bottom  = $parent.height() - $this.outerHeight();
-
-            if (pos.left >= pos.right && dx > 0) {
-              xOp = pos.right;
-            }
-            
-            if (pos.left <= 0 && dx <= 0) {
-              xOp = 0;
-            }
-
-            if (pos.top >= pos.bottom && dy > 0) {
-              yOp = pos.bottom;
-            }
-            
-            if (pos.top <= 0 && dy <= 0) {
-              yOp = 0;
-            }
-          }
+          var left    = ( dx >= 0 ? "+=" : "-=" ) + Math.abs(dx / self._scale)*mult;
+          var top     = ( dy >= 0 ? "+=" : "-=" ) + Math.abs(dy / self._scale)*mult;
 
           self._x     = curX;
           self._y     = curY;
-          self._xDir  = ( dx >= 0 ) ? 'right' : 'left';
-          self._yDir  = ( dy <= 0 ) ? 'up' : 'down';
+          self._xDir  = ( dx < 0 ) ? 'left' : 'right';
+          self._yDir  = ( dy < 0 ) ? 'up' : 'down';
 
+          if (self.options.constrainToParent) {
+            var pos   = $this.position(), x = curX - self._width, y = curY - self._height;
+            left = (x >= self._maxX || pos.left > self._maxX) ? self._maxX : (x <= 0 || pos.left < 0) ? 0 : left;
+            top = (y >= self._maxY || pos.top > self._maxY) ? self._maxY : (y <= 0 || pos.top < 0) ? 0 : top;
+            /*curX = x;
+            curY = y;*/
+          }
+          
           // fire user's start event.
           if ( !self._started && Math.abs(self._startX - curX) > 10 && Math.abs(self._startY - curY) > 10  ){
             self.options.start(event, self);
             self._started = true;
           }
           
-          $this.css({ top: yOp , left: xOp });
+          $this.css({ top: top , left: left });
           self._log( [self._moveTrigger, ", ", curX, " ", self._xDir, ", ", curY, " ", self._yDir].join('') );
           self._start = false;
         };
@@ -202,7 +194,6 @@
           _pepRequestAnimFrame(watchMoveLoop);
           if (self._moveEvent !== null ) dragging(self._moveEvent);
         })($, self, dragging);
-
 
       }
     };
@@ -228,8 +219,9 @@
 
     Pep.prototype._log = function(msg){
       if (this.options.debug){
-        if ( $('#msg').length == 0 ) $('#debug').append("<div id='msg'></div><div id='velocity'></div>");
-        $('#msg').html(msg);
+        var $msg = $('#msg');
+        if ( $msg.length == 0 ) $('#debug').append("<div id='msg'></div><div id='velocity'></div>");
+        $msg.html(msg);
         var vel = this._velocity();
         $('#velocity').html( "velocity: " + vel.x + ", " + vel.y + " " + "dt: " + this._dt );
       }
@@ -270,55 +262,53 @@
     })();
 
     Pep.prototype._ease = function(){
-      $this         = $(this.el);
+
+      var $this     = $(this.el);
+      var $parent   = $this.parent();
+      var pos       = $this.position();
       var vel       = this._velocity();
       var dt        = this._dt;
       var mult      = 1;
-      var x         = ( vel.x > 0 ) ? "+=" + vel.x * mult : "-=" + Math.abs(vel.x) * mult;
-      var y         = ( vel.y > 0 ) ? "+=" + vel.y * mult : "-=" + Math.abs(vel.y) * mult;
+      var x         = Math.abs(vel.x) * mult;
+      var y         = Math.abs(vel.y) * mult;
+      var left      = ( vel.x > 0 ? "+=" : "-=") + x;
+      var top       = ( vel.y > 0 ? "+=" : "-=") + y;
+      
+      if (this.options.constrainToParent) {
+        top = (pos.top - y <= 0) ? 0 : (y + pos.top >= this._maxY) ? this._maxY : top;
+        left = (pos.left - x <= 0) ? 0 : (x + pos.left >= this._maxX) ? this._maxX : left;
+      }
 
       // ✪  The CSS3 easing magic  ✪
       $this.css( this._cssEaseHash( this.options.cssEaseDuration, this.options.cssEaseString ) );
-      $this.css({ top: y, left: x });
+      $this.css({ top: top, left: left });
+      
     };
 
     Pep.prototype._cssEaseHash = function(time, params){
-      var transition = ['all ', time, 'ms ', params].join('');
-      return {
-                    '-webkit-transition'   : transition,  /* older webkit */
-                       '-moz-transition'   : transition,
-                        '-ms-transition'   : transition,
-                         '-o-transition'   : transition,
-                            'transition'   : transition,  /* custom */
-
-      '-webkit-transition-timing-function' : params,  /* older webkit */
-         '-moz-transition-timing-function' : params,
-         ' -ms-transition-timing-function' : params,
-           '-o-transition-timing-function' : params,
-              'transition-timing-function' : params   /* custom */
-            };
+      return this._setCssEaseHash(time, params);
     };
 
     Pep.prototype._cssEaseHashReset = function(){
-      return {
-                    '-webkit-transition'   : '',
-                       '-moz-transition'   : '',
-                        '-ms-transition'   : '',
-                         '-o-transition'   : '',
-                            'transition'   : '',
+      return this._setCssEaseHash();
+    };
 
-      '-webkit-transition-timing-function' : '',
-         '-moz-transition-timing-function' : '',
-         ' -ms-transition-timing-function' : '',
-           '-o-transition-timing-function' : '',
-              'transition-timing-function' : ''
-            };
+    Pep.prototype._setCssEaseHash = function (time, params) {
+      var transition, obj = {}, prefix;
+      params = params || '';
+      transition = params ? ['all ', time, 'ms ', params].join('') : '';
+      for (var i = 0, len = vendors.length; i < len; i++) {
+        prefix = vendors[i];
+        obj[prefix+'transition'] = transition;
+        obj[prefix+'transition-timing-function'] = params;
+      }
+      return obj;
     };
 
     Pep.prototype._doRest = function(event, obj){
       var self = this;
       this.timeout = setTimeout( function(){ self.options.rest(event, obj); }, self.options.cssEaseDuration );
-    }
+    };
 
     // A really lightweight plugin wrapper around the constructor,
     // preventing against multiple instantiations
