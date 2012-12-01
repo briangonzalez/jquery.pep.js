e78391a4f417363347f35cdca3a1c93116b24914
Brian Gonzalez <brian.gonzalez@geni.com>
2012-12-01 13:47:16 -0800

    added tests for toggling pep objects on & off

--- test/jquery.pep_test.js
+++ test/jquery.pep_test.js
@@ -39,4 +39,36 @@
     strictEqual($el.data('plugin_pep')._name, 'Pep', 'plugin name should be Pep');
   });
 
+  test('toggle pep object', 1, function() {
+    var $el = $( '#qunit-fixture span' ).first();
+    $el.pep();
+    $el.data('plugin_pep').toggle()
+    strictEqual($el.first().data('plugin_pep').disabled, true, 'this.disable variable should be true when toggled once');
+  });
+
+  test('explicit toggle pep object', 1, function() {
+    var $el = $( '#qunit-fixture span' ).first();
+    $el.pep();
+    $el.data('plugin_pep').toggle(false) // false === disable
+    strictEqual($el.first().data('plugin_pep').disabled, true, 'this.disable variable should be true when toggled off explicitly');
+  });
+
+  test('toggle via api - once', 1, function() {
+    var $el = $( '#qunit-fixture span' );
+    $el.pep();
+
+    $.pep.toggleAll()
+    strictEqual($el.first().data('plugin_pep').disabled, true, 'this.disable variable should be true when toggled once');
+  });
+
+  test('toggle via api - twice', 1, function() {
+    var $el = $( '#qunit-fixture span' );
+    $el.pep();
+
+    $.pep.toggleAll()
+    $.pep.toggleAll()
+    strictEqual($el.first().data('plugin_pep').disabled, false, 'this.disable variable should be false when toggled twice');
+  });
+
+  
 }(jQuery));
